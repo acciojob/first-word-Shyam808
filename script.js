f54031c24ae2128b5770d5eb9b21f0a189247e1c
Shyam Paghadal <shyampaghadal@gmail.com>
2025-06-19 09:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
-  // your code here
+  // Trim leading whitespace
+  s = s.trim();
+
+  // Find index of first space
+  const index = s.indexOf(' ');
+
+  // If there's no space, return the entire string
+  if (index === -1) return s;
+
+  // Otherwise, return the substring up to the first space
+  return s.substring(0, index);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
